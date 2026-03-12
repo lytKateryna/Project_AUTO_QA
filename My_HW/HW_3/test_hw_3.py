99c13fff0615d453b57d045a7b6b4ba5e159d969
lytKateryna <lyt.kateryna@gmail.com>
2026-03-12 19:46:27 +0100

    homework 3

--- My_HW/HW_3/test_hw_3.py
+++ My_HW/HW_3/test_hw_3.py
@@ -1,9 +1,9 @@
-from time import sleep
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 import pytest
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+
 
 
 @pytest.fixture
@@ -15,78 +15,67 @@ def driver():
     driver.quit()
 
 
-def test_find_to_element(driver):
-    driver.get("https://itcareerhub.de/ru")
-    logo = driver.find_element(By.CSS_SELECTOR, 'img[src*="Group_3793.svg"]')
+BASE_URL = "https://itcareerhub.de/ru"
+
+
+def test_find_logo(driver):
+    driver.get(BASE_URL)
+    wait = WebDriverWait(driver, 10)
+    logo = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'img[alt="IT Career Hub"]')))
     assert logo.is_displayed()
 
 
 def test_find_element_programm(driver):
-    driver.get("https://itcareerhub.de/ru")
-    sleep(2)
-    element = driver.find_element(By.XPATH,
-                                  "//*[contains(normalize-space(), 'Программы') or contains(normalize-space(), 'Programme')]")
+    driver.get(BASE_URL)
+    wait = WebDriverWait(driver, 10)
+    element = wait.until(EC.visibility_of_element_located((By.LINK_TEXT, 'Программы')))
     assert element.is_displayed()
 
 
-def test_find_element_preise(driver):
-    driver.get("https://itcareerhub.de/ru")
-    sleep(2)
-    element = driver.find_element(By.XPATH,
-                                  "//*[contains(normalize-space(), 'Способы оплаты') or contains(normalize-space(), 'Preise')]")
+def test_find_element_praise(driver):
+    driver.get(BASE_URL)
+    wait = WebDriverWait(driver, 10)
+    element = wait.until(EC.visibility_of_element_located((By.LINK_TEXT, 'Способы оплаты')))
     assert element.is_displayed()
 
 
-def test_find_element(driver):
-    driver.get("https://itcareerhub.de/ru")
-    sleep(2)
-    element = driver.find_element(By.XPATH,
-                                  "//*[contains(normalize-space(), 'Новости')]")
+def test_find_element_news(driver):
+    driver.get(BASE_URL)
+    wait = WebDriverWait(driver, 10)
+    element = wait.until(EC.visibility_of_element_located((By.LINK_TEXT, 'Блог')))
     assert element.is_displayed()
 
 
-def test_find_element_Über_uns(driver):
-    driver.get("https://itcareerhub.de/ru")
-    sleep(2)
-    element = driver.find_element(By.XPATH,
-                                  "//*[contains(normalize-space(), 'О нас') or contains(normalize-space(), 'Über_uns')]")
-    assert element.is_displayed()
-
-
-def test_find_element_Bewertungen(driver):
-    driver.get("https://itcareerhub.de/ru")
-    sleep(2)
-    element = driver.find_element(By.XPATH,
-                                  "//*[contains(normalize-space(), 'Отзывы') or contains(normalize-space(), 'Bewertungen')]")
+def test_find_element_o_nas(driver):
+    driver.get(BASE_URL)
+    wait = WebDriverWait(driver, 10)
+    element = wait.until(EC.visibility_of_element_located((By.LINK_TEXT, 'О нас')))
     assert element.is_displayed()
 
 
 def test_button(driver):
-    driver.get("https://itcareerhub.de/ru")
-
+    driver.get(BASE_URL)
     button_sub = driver.find_element(By.CSS_SELECTOR, 'a[href="/ru"]')
     button_sub.click()
 
-    sleep(2)
-
-    assert "https://itcareerhub.de/ru" in driver.current_url
+    assert BASE_URL in driver.current_url
 
 
 def test_phone_icon_text(driver):
     wait = WebDriverWait(driver, 20)
 
-    driver.get("https://itcareerhub.de/reviews")
+    driver.get("https://itcareerhub.de/ru/contact-us")
 
     phone_icon = wait.until(
-        EC.element_to_be_clickable((By.CSS_SELECTOR, 'a.tn-atom[href="#popup:form-tr3"]'))
+        EC.element_to_be_clickable((By.CSS_SELECTOR, 'a.tn-atom[href="#popup:form-tr"]'))
     )
     driver.execute_script("arguments[0].click();", phone_icon)
 
-    expected_text = "Если вы не дозвонились, заполните форму на сайте. Мы свяжемся с вами"
+    expected_text = "Свяжемся с вами в течение 15 минут, чтобы назначить время консультации"
 
     popup_text = wait.until(
-        EC.visibility_of_element_located((By.XPATH,
-                                          "//div[contains(@class, 't-popup')]//*[contains(text(), 'Если вы не дозвонились, заполните форму')]"))
+        EC.visibility_of_element_located((By.CLASS_NAME, "tn-group__1862496483175871291755985340"
+                                          ))
     )
 
     assert expected_text in popup_text.text
